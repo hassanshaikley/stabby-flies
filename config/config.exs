e07f04957be4cdbb792dd5792dbe9fdd64b29272
hassan shaikley <hassan.shaikley@gmail.com>
2018-11-18 12:14:59 -0700

    setup config for release

--- config/config.exs
+++ config/config.exs
@@ -27,4 +27,7 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+
+if (Mix.env() != "prod") do
+  import_config "#{Mix.env()}.exs"
+end
